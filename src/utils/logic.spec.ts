e53d5e7e9b1ed6822742ce546ba229ec7f35564d
Christian Sophocleous <c-sophocleous@hotmail.com>
2022-03-09 12:16:33 +0000

    tests added for helper functions

---
+++ src/utils/logic.spec.ts
@@ -0,0 +1,14 @@
+import { isFizz } from "./logic";
+import { isBuzz } from "./logic";
+
+test("isFuzz returns true if the number is divisible by 3", () => {
+    expect(isFizz(3)).toBe(true);
+    expect(isFizz(15)).toBe(true);
+    expect(isFizz(60)).toBe(true)
+})
+
+test("isBuzz returns true if the number is divisible by 5", () => {
+    expect(isBuzz(5)).toBe(true);
+    expect(isBuzz(15)).toBe(true);
+    expect(isBuzz(50)).toBe(true)
+})
