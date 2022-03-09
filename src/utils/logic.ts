f1e77aac59f2eb700b5a8cbfa8fe8b03095cd3b0
Christian Sophocleous <c-sophocleous@hotmail.com>
2022-03-09 12:18:01 +0000

    format with prettier

--- src/utils/logic.ts
+++ src/utils/logic.ts
@@ -1,14 +1,14 @@
-function isFizz(num: number): boolean | undefined{
-    if (num % 3 === 0){
-        return true
-    }
+function isFizz(num: number): boolean | undefined {
+  if (num % 3 === 0) {
+    return true;
+  }
 }
 
-function isBuzz(num: number): boolean | undefined{
-    if (num % 5 === 0){
-        return true
-    }
+function isBuzz(num: number): boolean | undefined {
+  if (num % 5 === 0) {
+    return true;
+  }
 }
 
-export {isFizz} 
-export {isBuzz}
+export { isFizz };
+export { isBuzz };
